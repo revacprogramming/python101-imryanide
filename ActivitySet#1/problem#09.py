34f89d73963b19ee578a00e194a1b470f2c83bd2
imryanide <rayanrafisheikh@gmail.com>
2022-05-11 23:10:12 +0530

    p9

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,3 +1,16 @@
 # Lists
 
-filename = "dataset/romeo.txt"
+filename = "romeo.txt"
+
+def get_words(filename):
+    l=[]
+    with open(filename) as f:
+        for line in f:
+            sentence = line.rstrip().split()
+            for word in sentence:
+                if word not in l:
+                    l.append(word)
+    l.sort()
+    return l
+
+print(get_words(filename))
