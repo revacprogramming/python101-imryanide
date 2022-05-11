ee046d4cea689582193ca56d9cf03dd7b2660d6d
imryanide <rayanrafisheikh@gmail.com>
2022-05-11 23:02:19 +0530

    p8

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,3 +1,18 @@
 # Files
 
-filename = "dataset/mbox-short.txt"
+
+
+filename = "mbox-short.txt"
+
+def get_avg(filename):
+    c = 0
+    total = 0
+    with open(filename) as f:
+        for line in f:
+            if line.startswith("X-DSPAM-Confidence:"):
+                c +=1
+                indx = line.find(':')
+                total += float(line[indx+1:])
+    return total/c
+
+print(f"Average spam confidence: {get_avg(filename)}")
