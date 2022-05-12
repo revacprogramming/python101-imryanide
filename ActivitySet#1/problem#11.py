93daae4e8f53f76fa49a327cd1fc646b0fc01b00
imryanide <rayanrafisheikh@gmail.com>
2022-05-12 21:51:46 +0530

    day2 p11

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,3 +1,31 @@
 # Tuples
 
-filename = "dataset/mbox-short.txt"
+from posixpath import split
+
+
+filename = "../dataset/mbox-short.txt"
+
+d = dict()
+
+def getmail(filename):
+    with open(filename) as f:
+        for lines in f:
+            word = lines.rstrip().split()
+            if 'From' in word and'From:' not in word:
+                email = word[1]
+                d[email] = d.get(email, 0) + 1
+
+    cnt =0
+    mail = None
+
+    for email,count in d.items():
+        if count>cnt:
+            cnt = count
+            mail = email
+
+    return mail, cnt
+
+x,y = getmail(filename)
+print(x,y)
+
+
