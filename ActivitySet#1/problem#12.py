e3f6b442321440c67199ddb75bd18fb8eaa0766e
arti <arti@pop-os.localdomain>
2022-07-02 19:58:56 +0530

    regex

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,2 +1,12 @@
 # Regular Expressions
 # https://www.py4e.com/lessons/regex
+import re
+f = open("../dataset/regex_sum_1543941.txt")
+
+s = 0
+for line in f:
+    w = re.findall(r"\d+", line)
+    if w:
+        for i in w:
+            s = s+int(i)
+print(s)
